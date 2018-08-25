13da4a4e056f14283f48dd0d9762b4d460ac8535
Ashwin Vaswani <ashwinv11@gmail.com>
2018-08-25 14:04:10 -0700

    add projects page

--- src/router.ts
+++ src/router.ts
@@ -14,11 +14,17 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/projects',
+      name: 'projects',
+      // route level code-splitting
+      // this generates a separate chunk (projects.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "projects" */ './views/Projects.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>Ashwin Vaswani is a creative coder. His work focus on web art, web apps & digital art. He produces art in the form of code, electronic music with voice & photography. Interested in connecting ‘the internet of things’ to modern art forms, Ashwin tries to find the balance between user experience & artistic representation. Proficient in modern web languages such as Ruby, JavaScript & PHP, his goal is to become an experienced full stack developer who can find the balance between art & technology. He supports open source software, privacy & net neutrality. Currently, he is interested in tasteful data visualization using the web.</p>
   </div>
 </template>
