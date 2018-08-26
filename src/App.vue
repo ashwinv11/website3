094eea4ce4c6edf289ec48edbcdffbc1357a08a2
Ashwin Vaswani <ashwinv11@gmail.com>
2018-08-25 17:05:03 -0700

    add fonts, finish home page content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="app">
+  <div id="app">
     <nav class="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/projects">Projects</router-link>
     </nav>
-    <router-view/>
+    <router-view class="app__content"/>
   </div>
 </template>
 
--- src/data/home.ts
+++ src/data/home.ts
@@ -1,8 +1,15 @@
 export const Data: object = {
   title: `Hi, I'm Ashwin`,
-  body: `Software Engineer at <a href="">Philosophie</a>,
-        with a BFA in Music Technology & the Digital Arts.
-        Previously Adjunct Faculty at CalArts and Web Engineer at Kadenze.
+  body: `<span class="italic">Software Engineer</span> at
+        <a href="https://philosophie.is/" target="_blank" class="italic">Philosophie</a>,
+        with a <span class="italic">BFA in
+        <a href="https://mtiid.calarts.edu/" target="_blank">Music Technology</a>
+        & the Digital Arts.</span>
+        Previously <span class="italic">Adjunct Faculty</span> at
+        <a href="https://calarts.edu/" target="_blank" class="italic">CalArts</a> and
+        <span class="italic">Web Engineer</span> at
+        <a href="https://kadenze.com/" target="_blank" class="italic">Kadenze</a>.
         Feel free to explore my website for my projects, music, and other work.
-        Oh also, this website is entirely open source, which you can find here.`,
+        Oh also, this website is entirely open source, which you can find
+        <a href="https://github.com/ashwinv11/website3" target="_blank">here<a>.`,
 }
