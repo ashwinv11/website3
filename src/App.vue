28729f43487df859afd5e9e9cd79725b638cc2e7
Ashwin Vaswani <ashwinv11@gmail.com>
2018-08-26 13:20:43 -0700

    add page layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    <nav class="nav">
+  <div id="app" class="app">
+    <nav class="app__nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/projects">Projects</router-link>
     </nav>
-    <router-view class="app__content"/>
+    <main class="app__content">
+      <router-view />
+    </main>
+    <footer class="app__footer"><span>Test</span></footer>
   </div>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <p>Ashwin Vaswani is a creative coder. His work focus on web art, web apps & digital art. He produces art in the form of code, electronic music with voice & photography. Interested in connecting ‘the internet of things’ to modern art forms, Ashwin tries to find the balance between user experience & artistic representation. Proficient in modern web languages such as Ruby, JavaScript & PHP, his goal is to become an experienced full stack developer who can find the balance between art & technology. He supports open source software, privacy & net neutrality. Currently, he is interested in tasteful data visualization using the web.</p>
+    <h1>{{$options.json.title}}</h1>
+    <p v-html="$options.json.body"></p>
   </div>
 </template>
+
+<script lang="ts">
+import { Data } from '../data/about'
+
+export default {
+  json: Data,
+}
+</script>
