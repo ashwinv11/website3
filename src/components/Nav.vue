3cf225befd54639980741a0d861cd3c188ede39f
Ashwin Vaswani <ashwinv11@gmail.com>
2018-08-27 20:40:49 -0700

    style changes to layout

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,7 @@
 <template>
   <nav class="app__nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/projects">Projects</router-link> |
+    <router-link to="/">About</router-link>
+    <router-link to="/projects">Projects</router-link>
     <a href="/assets/Vaswani_Ashwin_Resume.pdf">Resume</a>
   </nav>
 </template>
--- src/data/projects/aries.ts
+++ src/data/projects/aries.ts
@@ -2,7 +2,7 @@ import { Project } from '@/interfaces/project'
 
 export const Aries: Project = {
   slug: 'aries-music',
-  title: 'Aries Music',
+  title: `Aries' Music`,
   date: 'Jan 10, 2015',
   body: `<iframe align="middle" frameborder="no" height="350" scrolling="no"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43467142&color=d13737&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=true" width="100%"></iframe>`,
