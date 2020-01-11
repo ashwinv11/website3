d8a76e51df4b193dbd9284d2c4114d806610bea6
Ashwin Vaswani <ashwinv11@gmail.com>
2020-01-10 23:52:00 -0800

    bump job

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,14 +1,14 @@
 export const Data: object = {
   title: `Hi, I'm Ashwin`,
-  body: `<span class="italic">Software Engineer</span> at
-        <a href="https://philosophie.is/" target="_blank" class="italic">Philosophie</a>,
+  body: `<span class="italic">Senior Software Engineer</span> at
+        <a href="https://www.sweetgreen.com/" target="_blank" class="italic">Sweetgreen</a>,
         with a <span class="italic">BFA in
         <a href="https://mtiid.calarts.edu/" target="_blank">Music Technology</a>
         & the Digital Arts.</span>
-        Previously <span class="italic">Adjunct Faculty</span> at
-        <span class="italic">CalArts</span> and
-        <span class="italic">Web Engineer</span> at
-        <span class="italic">Kadenze</span>.`,
+        Previously <span class="italic">Software Engineer</span> at
+        <span class="italic">Philosophie</span> and
+        <span class="italic">Adjunct Faculty</span> at
+        <span class="italic">CalArts</span>.`,
 }
 
 const synopsis = `colorful. creative. coder. Ashwin Vaswani is a Software Engineer based in Los Angeles, California. Proficient in languages like Ruby, JavaScript, and PHP, he's interested in modern full stack web development and solving human problems through code.`
