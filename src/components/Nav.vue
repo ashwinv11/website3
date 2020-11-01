3f1c9f005984e5d1991cc46e27358fef0a2f2393
Ashwin Vaswani <ashwinv11@gmail.com>
2020-10-31 22:27:40 -0300

    bump resume

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,6 +2,11 @@
   <nav class="app__nav">
     <router-link to="/" exact class="italic">About</router-link>
     <router-link to="/projects" class="italic">Projects</router-link>
-    <a href="/assets/Vaswani_Ashwin_Resume.pdf" target="_blank" class="italic">Resume</a>
+    <a
+      href="/assets/Vaswani_Ashwin_Resume.pdf?v=3.0"
+      target="_blank"
+      class="italic"
+      >Resume</a
+    >
   </nav>
 </template>
